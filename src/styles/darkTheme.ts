486a5b8dd637d9cab343d7be98c754e69b413985
paichato <tsenane14@gmail.com>
2022-04-14 18:56:55 +0200

    added darktheme

---
+++ src/styles/darkTheme.ts
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    white: "#ffffff",
+    darker_blue: "#508bfc",
+    main_color: "#1967FB",
+    title: "#3D434D",
+    text: "#888D97",
+    yellow: "#FFCC00",
+    border: "#e3e4e5",
+    bg: "#000000",
+  },
+};
---
+++ src/styles/lightTheme.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    white: "#ffffff",
+    darker_blue: "#508bfc",
+    main_color: "#1967FB",
+    title: "#3D434D",
+    text: "#888D97",
+    yellow: "#FFCC00",
+    border: "#e3e4e5",
+    bg: "#F2F3F5",
+    error: "#E83F5B",
+  },
+};
