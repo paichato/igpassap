6cdc98650a9c1f848ade4f5ac185c4d4882ce608
paichato <tsenane14@gmail.com>
2022-04-14 18:12:26 +0200

    added them file +

--- App.tsx
+++ App.tsx
@@ -1,35 +1,39 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import React from "react";
+import { StatusBar } from "react-native";
+import AppLoading from "expo-app-loading";
 import {
   useFonts,
   Rubik_300Light,
   Rubik_400Regular,
-  Rubik_500Medium
-} from '@expo-google-fonts/rubik';
-import { NavigationContainer } from '@react-navigation/native';
+  Rubik_500Medium,
+} from "@expo-google-fonts/rubik";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { AppRoutes } from './src/routes/app.routes';
+import { AppRoutes } from "./src/routes/app.routes";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/styles/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Rubik_300Light,
     Rubik_400Regular,
-    Rubik_500Medium
+    Rubik_500Medium,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
     <NavigationContainer>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <AppRoutes />
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <AppRoutes />
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
