cf95be8d895dc3978b43ca8b8ec15f774786d18c
paichato <tsenane14@gmail.com>
2022-04-14 18:11:40 +0200

    added them file

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    white: "#ffffff",
+    main_color: "#508bfc",
+    title: "#3D434D",
+    text: "#888D97",
+    yellow: "#FFCC00",
+    border: "#e3e4e5",
+  },
+};
